492768ceb660f4ecbcf6f650a2ce5b64f3fe31cb
HuongNguyenQuoc <quochuong05ln@gmail.com>
2026-08-05 23:32:08 +0700

    Register all controllers, services, entity in paste.module

--- src/paste/paste.module.ts
+++ src/paste/paste.module.ts
@@ -1,4 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { RedisSerivce } from 'src/paste/cache/redis.service';
+import { PasteController } from 'src/paste/paste.controller';
+import { Paste } from 'src/paste/paste.entity';
+import { PasteService } from 'src/paste/paste.service';
+import { ShortenerService } from 'src/paste/shortener/shortener.service';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([Paste])],
+  controllers: [PasteController],
+  providers: [RedisSerivce, ShortenerService, PasteService],
+})
 export class PasteModule {}
