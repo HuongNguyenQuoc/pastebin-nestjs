8d7fbd33a6a2f06d05fe8db6836e30803164dbe3
HuongNguyenQuoc <quochuong05ln@gmail.com>
2026-08-06 13:44:13 +0700

    init dataSource use own CLI because we want run in production environment

---
+++ src/data-source.ts
@@ -0,0 +1,18 @@
+import { config } from 'dotenv';
+import { Paste } from 'src/paste/paste.entity';
+import { DataSource } from 'typeorm';
+import 'reflect-metadata';
+
+config();
+
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: process.env.DATABASE_HOST,
+  port: Number(process.env.DATABASE_PORT),
+  username: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  entities: [Paste],
+  migrations: ['src/migrations/*.ts'],
+  synchronize: false, // Set to false in production to avoid data loss
+});
