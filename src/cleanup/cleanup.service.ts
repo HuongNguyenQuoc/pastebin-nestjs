5501df5e3c6367f20e718a9591a747e79539a527
HuongNguyenQuoc <quochuong05ln@gmail.com>
2026-08-06 09:51:39 +0700

    Turn on validationPipe + Swagger and Cleanup job

---
+++ src/cleanup/cleanup.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@nestjs/common';
+import { Cron, CronExpression } from '@nestjs/schedule';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Paste } from 'src/paste/paste.entity';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class CleanupService {
+  constructor(
+    @InjectRepository(Paste)
+    private readonly pasteRepo: Repository<Paste>,
+  ) {}
+
+  @Cron(CronExpression.EVERY_10_MINUTES)
+  async deleteExpiredPastes() {
+    await this.pasteRepo.delete(`
+      DELETE FROM pastes
+      WHERE expiration_length_in_minutes IS NOT NULL
+      AND created_at + (expiration_length_in_minutes || ' minutes')::interval < NOW()
+    `);
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true })); // Enable validation and transformation for incoming requests
+
+  // Use ConfigService to read PORT from environment (.env)
+  const configService = app.get(ConfigService);
+  const port = Number(configService.get<number>('PORT'));
+
+  const config = new DocumentBuilder()
+    .setTitle('Pastebin Clone API (NestJS)')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document); // Set up Swagger documentation at the /docs endpoint
+  try {
+    await app.listen(port);
+  } catch (error) {
+    console.error('Error occurred while starting the server:', error);
+    process.exit(1); // Exit the process with an error code
+  }
 }
-bootstrap();
+void bootstrap();
